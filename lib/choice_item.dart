8db2fecfe746dbbcc8e11bbb3cb309f36c0d4fbc
Rémy Machavoine <remygt@hotmail.fr>
2022-12-15 22:06:02 +0100

    add foreach on footer list

--- lib/choice_item.dart
+++ lib/choice_item.dart
@@ -1,11 +1,25 @@
 import 'package:flutter/material.dart';
 
 class ChoiceItem extends StatelessWidget {
-  const ChoiceItem({Key? key}) : super(key: key);
+  final String text;
+
+  const ChoiceItem({Key? key, required this.text}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+      return Container(
+        height: 30,
+        decoration: const BoxDecoration(
+          color: Colors.red,
+          shape: BoxShape.circle,
+        ),
+        child: Text(
+          text,
+          style: const TextStyle(
+            color: Colors.grey,
+            fontSize: 20,
+          ),
+        ),
+      );
   }
 }
--- lib/footer.dart
+++ lib/footer.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:td_flutter/choice_item.dart';
 
 class Footer extends StatelessWidget {
   final double height;
+  final List<String> hobbies = [
+    "Football",
+    "Basketball",
+    "Volleyball",
+    "Tennis",
+    "Chess",
+    "Ping Pong",
+    "Bodybuilding",
+  ];
 
-  const Footer({Key? key, required this.height}) : super(key: key);
+  Footer({Key? key, required this.height}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -11,15 +21,16 @@ class Footer extends StatelessWidget {
       height: height,
       width: double.maxFinite,
       color: Colors.white,
-      child: const Padding(
-          padding: EdgeInsets.all(8.0),
-          child: Text(
-            'Footer',
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 30,
-            ),
-          )),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Wrap(
+          spacing: 10,
+          runSpacing: 10,
+          children: hobbies.map((element) {
+            return ChoiceItem(text: element);
+          }).toList(),
+        ),
+      ),
     );
   }
 }
